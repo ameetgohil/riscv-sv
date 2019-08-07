341f3aaac9225024eeb4babab7698b5ab1a17d0c
qubits <ameet@siglabs.com>
2019-08-07 00:11:22 -0700

    fix zero read

--- rtl/dpram_32_ba.sv
+++ rtl/dpram_32_ba.sv
@@ -38,6 +38,9 @@ module dpram_32_ba
    reg [7:0]                        mem2 [DEPTH] /* synthesis syn_ramstyle = "block_ram" */;
    reg [7:0]                        mem3 [DEPTH] /* synthesis syn_ramstyle = "block_ram" */;
 
+   reg [3:0][7:0]            p0_dout;
+   reg [3:0][7:0]            p1_dout;
+   
    /* verilator lint_off WIDTH */
    initial begin
       $readmemh(MEM0_INIT, mem0);
@@ -63,49 +66,57 @@ module dpram_32_ba
 
 `ifdef ZERO_READ
    assign i_p0_data[0] = mem0[addr0];
+`else
+   assign i_p0_data[0] = p0_dout[0];
 `endif
    
    always @(posedge clk) begin
-      i_p0_data[0] <= mem0[addr0];
+      p0_dout[0] <= mem0[addr0];
       if(t_p0_mask[0] & t_p0_valid & t_p0_we) begin
          mem0[addr0] <= t_p0_data[0];
-         i_p0_data[0] <= t_p0_data[0];
+         p0_dout[0] <= t_p0_data[0];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p0_data[1] = mem1[addr0];
+`else
+   assign i_p0_data[1] = p0_dout[1];
 `endif
    
    always @(posedge clk) begin
-      i_p0_data[1] <= mem1[addr0];
+      p0_dout[1] <= mem1[addr0];
       if(t_p0_mask[1] & t_p0_valid & t_p0_we) begin
          mem1[addr0] <= t_p0_data[1];
-         i_p0_data[1] <= t_p0_data[1];
+         p0_dout[1] <= t_p0_data[1];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p0_data[2] = mem2[addr0];
+`else
+   assign i_p0_data[2] = p0_dout[2];
 `endif
    
    always @(posedge clk) begin
-      i_p0_data[2] <= mem2[addr0];
+      p0_dout[2] <= mem2[addr0];
       if(t_p0_mask[2] & t_p0_valid & t_p0_we) begin
          mem2[addr0] <= t_p0_data[2];
-         i_p0_data[2] <= t_p0_data[2];
+         p0_dout[2] <= t_p0_data[2];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p0_data[3] = mem3[addr0];
+`else
+   assign i_p0_data[3] = p0_dout[3];
 `endif
    
    always @(posedge clk) begin
-      i_p0_data[3] <= mem3[addr0];
+      p0_dout[3] <= mem3[addr0];
       if(t_p0_mask[3] & t_p0_valid & t_p0_we) begin
          mem3[addr0] <= t_p0_data[3];
-         i_p0_data[3] <= t_p0_data[3];
+         p0_dout[3] <= t_p0_data[3];
       end
    end
    
@@ -121,50 +132,58 @@ module dpram_32_ba
 
 `ifdef ZERO_READ
    assign i_p1_data[0] = mem0[addr1];
+`else
+   assign i_p1_data[0] = p1_dout[0];
 `endif
 
    
    always @(posedge clk) begin
-      i_p1_data[0] <= mem0[addr1];
+      p1_dout[0] <= mem0[addr1];
       if(t_p1_mask[0] & t_p1_valid & t_p1_we) begin
          mem0[addr1] <= t_p1_data[0];
-         i_p1_data[0] <= t_p1_data[0];
+         p1_dout[0] <= t_p1_data[0];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p1_data[1] = mem1[addr1];
+`else
+   assign i_p1_data[1] = p1_dout[1];
 `endif
    
    always @(posedge clk) begin
-      i_p1_data[1] <= mem1[addr1];
+      p1_dout[1] <= mem1[addr1];
       if(t_p1_mask[1] & t_p1_valid & t_p1_we) begin
          mem1[addr1] <= t_p1_data[1];
-         i_p1_data[1] <= t_p1_data[1];
+         p1_dout[1] <= t_p1_data[1];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p1_data[2] = mem2[addr1];
+`else
+   assign i_p1_data[2] = p1_dout[2];
 `endif
    
    always @(posedge clk) begin
-      i_p1_data[2] <= mem2[addr1];
+      p1_dout[2] <= mem2[addr1];
       if(t_p1_mask[2] & t_p1_valid & t_p1_we) begin
          mem2[addr1] <= t_p1_data[2];
-         i_p1_data[2] <= t_p1_data[2];
+         p1_dout[2] <= t_p1_data[2];
       end
    end
 
 `ifdef ZERO_READ
    assign i_p1_data[3] = mem3[addr1];
+`else
+   assign i_p1_data[3] = p1_dout[3];
 `endif
    
    always @(posedge clk) begin
-      i_p1_data[3] <= mem3[addr1];
+      p1_dout[3] <= mem3[addr1];
       if(t_p1_mask[3] & t_p1_valid & t_p1_we) begin
          mem3[addr1] <= t_p1_data[3];
-         i_p1_data[3] <= t_p1_data[3];
+         p1_dout[3] <= t_p1_data[3];
       end
    end
 
