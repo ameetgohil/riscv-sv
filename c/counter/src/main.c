c5cbfe7519af74cb0c4a7095858afda474e9c1d9
Ameet Gohil <qubits1@gmail.com>
2020-03-03 22:40:57 -0800

    update counter to have more branches for debug

--- c/counter/src/main.c
+++ c/counter/src/main.c
@@ -1,11 +1,16 @@
 int main() {
     register volatile unsigned int x3 asm("x3");
-    
-    int count = 0;
-    while(1) {
+    volatile unsigned int *p;
+    int count = 0xA;
+    while(count < 100) {
 	//	if(count < 10)
+      if(count < 20)
 	count++;
-	x3=count;
+      else
+	count += 2;
+      x3=count;
+      *p=count;
     }
+    while(1);
     return 0;
 }
