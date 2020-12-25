c3816e0aca7b2d286056fe51c3597f5faa65aa9f
Ameet Gohil <qubits1@gmail.com>
2020-12-24 19:19:17 -0800

    add inc function -verifies jalr instruction

--- c/counter/src/main.c
+++ c/counter/src/main.c
@@ -1,10 +1,15 @@
+int inc(int count) {
+    return ++count;
+}
+
 int main() {
     register volatile unsigned int x3 asm("x3");
     
     int count = 0;
     while(1) {
 	//	if(count < 10)
-	count++;
+	count = inc(count);
+	//count++;
 	x3=count;
     }
     return 0;
